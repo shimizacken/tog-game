d88959075e9ed1f2ad6ab2276aa7242c980a5d63
shimizacken <shimizacken@gmail.com>
2019-04-16 19:35:35 +0200

    first commit

---
+++ src/features/driveDashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.module.scss';
+
+const DrivingDashboardContainer = () => {
+
+    return <div class={styles.root}>Driving dashboard</div>;
+};
+
+DrivingDashboardContainer.propTypes = {
+
+};
+
+export default DrivingDashboardContainer;
