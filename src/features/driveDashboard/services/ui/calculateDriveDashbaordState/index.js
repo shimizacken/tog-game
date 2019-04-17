cd59a285279ed4f23860e1161711e752b3ff9da0
shimizacken <shimizacken@gmail.com>
2019-04-17 06:16:22 +0200

    calculateDrivingStatus refactored

--- src/features/driveDashboard/services/ui/calculateDriveDashbaordState/index.js
+++ src/features/driveDashboard/services/ui/calculateDriveDashbaordState/index.js
@@ -2,13 +2,6 @@ import { DrivingButtonStates } from '../drivingButtonStates';
 
 const calculateDrivingStatus = (drivingButtonState = DrivingButtonStates.READY, throttleSpeed = 0) => {
 
-	if(drivingButtonState === DrivingButtonStates.ERROR 
-          || drivingButtonState === DrivingButtonStates.TURNED_OFF
-          || drivingButtonState === DrivingButtonStates.READY) {
-      
-        return false;
-    }
-    
     if(drivingButtonState === DrivingButtonStates.DRIVE && throttleSpeed > 0) {
     
         return true;
