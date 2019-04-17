01c19686aebc36ae25c6979492d640395473d82c
shimizacken <shimizacken@gmail.com>
2019-04-17 18:47:54 +0200

    minor change

--- src/features/driveDashboard/services/ui/calculateShakeClassName/index.js
+++ src/features/driveDashboard/services/ui/calculateShakeClassName/index.js
@@ -1,29 +1,29 @@
 const calculateShakeClassName = (drivingStatus, throttleSpeed, styles) => {
 
-    if(drivingStatus === false) {
-        return;
-    }
-
     let style;
 
+    if(drivingStatus === false) {
+        return style;
+    }
+
     if(throttleSpeed > 80 && throttleSpeed <= 100) {
-        style = styles.shakeveryveryfast;
+        return styles.shakeveryveryfast;
     }
 
     if(throttleSpeed > 60 && throttleSpeed <= 80) {
-        style = styles.shakeveryfast;
+        return styles.shakeveryfast;
     }
     
     if(throttleSpeed > 40 && throttleSpeed <= 60) {
-        style = styles.shakefast;
+        return styles.shakefast;
     }
 
     if(throttleSpeed > 20 && throttleSpeed <= 40) {
-        style = styles.shakeslow;
+        return styles.shakeslow;
     }
 
     if(throttleSpeed > 0 && throttleSpeed <= 20) {
-        style = styles.shakeveryslow;
+        return styles.shakeveryslow;
     }
 
     return style;
