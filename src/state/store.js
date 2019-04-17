3348ca71cbd87008f4c9137f49048e4fbe3414ae
shimizacken <shimizacken@gmail.com>
2019-04-17 06:42:50 +0200

    - middleware applied with store enhancer - store connected

--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import middlewares from './middlewares'
 
-const store = createStore();
+const composeEnhancers = process.env.NODE_ENV === 'development'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  : compose;
+
+export const store = createStore(reducers, composeEnhancers(applyMiddleware(...middlewares)));
 
 export default store;
