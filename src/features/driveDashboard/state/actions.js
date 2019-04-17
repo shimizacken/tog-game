0f51c3de14d6dfc9260f31d7747b2b0159ae9ec1
shimizacken <shimizacken@gmail.com>
2019-04-17 11:19:51 +0200

    setDrivingButtonSwitchingState action added

--- src/features/driveDashboard/state/actions.js
+++ src/features/driveDashboard/state/actions.js
@@ -1,8 +1,14 @@
-import { SET_DRIVE_BUTTON_STATUS, SET_THROTTLE_SPEED } from "./constants";
+import { SET_DRIVE_BUTTON_STATUS, SET_THROTTLE_SPEED, SET_DRIVE_BUTTON_SWITCHING_STATUS } from "./constants";
+import DrivingButtonStates from "../services/ui/enums/drivingButtonStates";
 
 export const setDrivingButtonState = drivingButtonState => ({
   type: SET_DRIVE_BUTTON_STATUS,
   drivingButtonState
+});
+
+export const setDrivingButtonSwitchingState = () => ({
+  type: SET_DRIVE_BUTTON_SWITCHING_STATUS,
+  drivingButtonState: DrivingButtonStates.SWITCHING
 });
 
 export const setThrottleSpeed = throttleSpeed => ({
