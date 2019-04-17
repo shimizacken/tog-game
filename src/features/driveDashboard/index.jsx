da530aa5f172e0068458aa02ff12d9f6e00817ce
shimizacken <shimizacken@gmail.com>
2019-04-17 13:21:54 +0200

    animation tuned

--- src/features/driveDashboard/index.jsx
+++ src/features/driveDashboard/index.jsx
@@ -16,12 +16,35 @@ const DrivingDashboardContainer = ({drivingButtonState, throttleSpeed, setDrivin
 
     const drivingStatus = calculateDrivingStatus(drivingButtonState, throttleSpeed);
     
+    const getClassName = () => {
+
+        if(drivingStatus === false) {
+            return;
+        }
+
+        let style;
+
+        if(throttleSpeed > 60 && throttleSpeed <= 100) {
+            style = styles.shakeveryfast;
+        }
+        
+        if(throttleSpeed > 40 && throttleSpeed < 60) {
+            style = styles.shakefast;
+        }
+
+        if(throttleSpeed > 10 && throttleSpeed < 40) {
+            style = styles.shakeslow;
+        }
+
+        if(throttleSpeed > 0 && throttleSpeed < 10) {
+            style = styles.shakeveryslow;
+        }
+
+        return style;
+    };
+
     return (
-        <div className={
-            classNames(
-                (drivingStatus === true && throttleSpeed < 50) && styles.shakeslow,
-                (drivingStatus === true && throttleSpeed >= 50) && styles.shakefast
-            )}
+        <div className={getClassName()}
         >
         <div>
             <h1>
