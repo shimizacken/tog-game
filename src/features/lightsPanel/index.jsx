26bed942b7ea7e6409309cb900d82d39086e81b9
shimizacken <shimizacken@gmail.com>
2019-04-27 21:24:30 +0200

    LightsPanelContainer added

--- src/features/lightsPanel/index.jsx
+++ src/features/lightsPanel/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Panel from '../../components/panel';
+import Button from '../../components/button';
+import styles from './index.module.scss';
 
-const LightsPanelContainer = ({lights}) => <div>{lights.frontLightsState}</div>;
+const LightsPanelContainer = ({lights}) => 
+    <Panel>
+        <Button className={styles.button} />
+        <Button className={styles.button} />
+        <Button className={styles.button} />
+        <Button className={styles.button} />
+    </Panel>;
 
 const mapStateToProps = state => ({
     lights: state.lights
