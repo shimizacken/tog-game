27d47324b223235f766698529f452213b3e45a97
shimizacken <shimizacken@gmail.com>
2019-04-27 22:03:12 +0200

    dispaches integrarted

--- src/features/lightsPanel/index.jsx
+++ src/features/lightsPanel/index.jsx
@@ -10,29 +10,27 @@ import {
 } from './state/actions';
 import styles from './index.module.scss';
 
-const getButton = () => (text, onClick) => <Button className={styles.button} text={text} onClick={onClick} />;
-
 const LightsPanelContainer = (
     {toggleFrontLights,
     toggleBackLights,
     toggleCabinLights,
     toggleWagonsLights}) => 
     <Panel>
-        {getButton('Front', toggleFrontLights)}
-        {getButton('Back', toggleBackLights)}
-        {getButton('Cabin', toggleCabinLights)}
-        {getButton('Wagons', toggleWagonsLights)}
+        <Button className={styles.button} text={'Front'} onClick={toggleFrontLights} />
+        <Button className={styles.button} text={'Back'} onClick={toggleBackLights} />
+        <Button className={styles.button} text={'Cabin'} onClick={toggleCabinLights} />
+        <Button className={styles.button} text={'Wagons'} onClick={toggleWagonsLights} />
     </Panel>;
 
 const mapStateToProps = state => ({
     lights: state.lights
 });
 
-const mapDispatchToProps = ({
-    toggleFrontLights: setFrontLightState,
-    toggleBackLights: setBackLightState,
-    toggleCabinLights: setCabinLightState,
-    toggleWagonsLights: setWagonsLightState
+const mapDispatchToProps = dispatch => ({
+    toggleFrontLights: () => dispatch(setFrontLightState),
+    toggleBackLights: () => dispatch(setBackLightState),
+    toggleCabinLights: () => dispatch(setCabinLightState),
+    toggleWagonsLights: () => dispatch(setWagonsLightState)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(LightsPanelContainer);
