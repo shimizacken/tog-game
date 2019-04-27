2bf29eceb37df631042019b88a1fa0ab34787de7
shimizacken <shimizacken@gmail.com>
2019-04-27 21:53:01 +0200

    state refactored

--- src/features/lightsPanel/state/reducers.js
+++ src/features/lightsPanel/state/reducers.js
@@ -1,11 +1,10 @@
-import { LightStates } from "../bl/enums";
 import { SET_FRONT_LIGHT_STATE, SET_BACK_LIGHT_STATE, SET_CABIN_LIGHT_STATE, SET_WAGON_LIGHT_STATE } from "./constants";
 
 const lightsInitialState = {
-    frontLightsState: LightStates.OFF,
-    backLightsState: LightStates.OFF,
-    cabinLightsState: LightStates.OFF,
-    wagonsLightsState: LightStates.OFF
+    frontLightsState: false,
+    backLightsState: false,
+    cabinLightsState: false,
+    wagonsLightsState: false
 };
 
 const lights = (state = lightsInitialState, action) => {
@@ -13,28 +12,28 @@ const lights = (state = lightsInitialState, action) => {
     if (action.type === SET_FRONT_LIGHT_STATE) {
         return {
             ...state,
-            frontLightsState: action.frontLightsState
+            frontLightsState: !state.frontLightsState
         };
     }
 
     if (action.type === SET_BACK_LIGHT_STATE) {
         return {
             ...state,
-            backLightsState: action.backLightsState
+            backLightsState: !state.backLightsState
         };
     }
 
     if (action.type === SET_CABIN_LIGHT_STATE) {
         return {
             ...state,
-            cabinLightsState: action.cabinLightsState
+            cabinLightsState: !state.cabinLightsState
         };
     }
 
     if (action.type === SET_WAGON_LIGHT_STATE) {
         return {
             ...state,
-            wagonsLightsState: action.wagonsLightsState
+            wagonsLightsState: !state.wagonsLightsState
         };
     }
 
