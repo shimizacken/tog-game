f0d219039a4a3d0e0fa23b4d1e2ff65c3160d4ff
shimizacken <shimizacken@gmail.com>
2019-04-27 21:23:28 +0200

    button refactored

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -3,19 +3,27 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import styles from './index.module.scss';
 
-const Button = React.memo(({className, onClick}) => {
+const Button = React.memo(({className, text, onClick}) => {
 
     return (
         <button 
             className={classNames(styles.root, className)}
             onClick={onClick}
         >
+        {text}
         </button>);
 });
 
 Button.propTypes = {
-    className: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    className: PropTypes.string,
+    text:  PropTypes.string,
+    onClick: PropTypes.func
+};
+
+Button.defaultTypes = {
+    className: undefined,
+    text:  undefined,
+    onClick: undefined
 };
 
 export default Button;
