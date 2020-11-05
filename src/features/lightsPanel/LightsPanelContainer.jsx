863d5a864da245c0c4afea7b6c28599f5e6cb01c
shimizacken <shimizacken@gmail.com>
2020-11-05 22:01:18 +0100

    refactored

--- src/features/lightsPanel/LightsPanelContainer.jsx
+++ src/features/lightsPanel/LightsPanelContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Panel } from "../../components/panel/Panel";
 import { LightButton } from "./components/lightButton/LightButton";
 import {
@@ -10,57 +10,41 @@ import {
 } from "./state/actions";
 import styles from "./LightsPanelContainer.module.scss";
 
-const LightsPanelContainer = ({
-  frontLightsState,
-  backLightsState,
-  cabinLightsState,
-  wagonsLightsState,
-  toggleFrontLights,
-  toggleBackLights,
-  toggleCabinLights,
-  toggleWagonsLights,
-}) => (
-  <Panel classNameInnerWrapper={styles.buttons} width={"200"} height={"200"}>
-    <LightButton
-      enabled={frontLightsState}
-      text={"Front"}
-      onClick={toggleFrontLights}
-    />
-    <LightButton
-      enabled={backLightsState}
-      text={"Back"}
-      onClick={toggleBackLights}
-    />
-    <LightButton
-      enabled={cabinLightsState}
-      text={"Cabin"}
-      onClick={toggleCabinLights}
-    />
-    <LightButton
-      enabled={wagonsLightsState}
-      text={"Wagons"}
-      onClick={toggleWagonsLights}
-    />
-  </Panel>
-);
+export const LightsPanelContainer = () => {
+  const dispatch = useDispatch();
+  const frontLightsState = useSelector(
+    (state) => state.lights.frontLightsState
+  );
+  const backLightsState = useSelector((state) => state.lights.backLightsState);
+  const cabinLightsState = useSelector(
+    (state) => state.lights.cabinLightsState
+  );
+  const wagonsLightsState = useSelector(
+    (state) => state.lights.wagonsLightsState
+  );
 
-const mapStateToProps = (state) => ({
-  frontLightsState: state.lights.frontLightsState,
-  backLightsState: state.lights.backLightsState,
-  cabinLightsState: state.lights.cabinLightsState,
-  wagonsLightsState: state.lights.wagonsLightsState,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  toggleFrontLights: () => dispatch(setFrontLightState),
-  toggleBackLights: () => dispatch(setBackLightState),
-  toggleCabinLights: () => dispatch(setCabinLightState),
-  toggleWagonsLights: () => dispatch(setWagonsLightState),
-});
-
-const connected = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(LightsPanelContainer);
-
-export { connected as LightsPanelContainer };
+  return (
+    <Panel classNameInnerWrapper={styles.buttons} width={"200"} height={"200"}>
+      <LightButton
+        enabled={frontLightsState}
+        text={"Front"}
+        onClick={() => dispatch(setFrontLightState)}
+      />
+      <LightButton
+        enabled={backLightsState}
+        text={"Back"}
+        onClick={() => dispatch(setBackLightState)}
+      />
+      <LightButton
+        enabled={cabinLightsState}
+        text={"Cabin"}
+        onClick={() => dispatch(setCabinLightState)}
+      />
+      <LightButton
+        enabled={wagonsLightsState}
+        text={"Wagons"}
+        onClick={() => dispatch(setWagonsLightState)}
+      />
+    </Panel>
+  );
+};
