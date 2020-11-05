d658219a0f9b2a0cc796c44fff6c7d38ed7aad46
shimizacken <shimizacken@gmail.com>
2020-11-05 22:51:09 +0100

    migrated

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,7 +4,7 @@ import styles from "./Button.module.scss";
 
 export const Button: React.FC<
   React.ComponentProps<"button"> & {
-    text: string;
+    text?: string;
     onClick: () => void;
   }
 > = ({ text, className, onClick }) => (
